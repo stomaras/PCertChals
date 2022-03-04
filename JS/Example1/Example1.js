eade2173b37b75c30486a313b2af9e05fcf31983
stomaras <sptoma724@gmail.com>
2022-03-04 17:05:25 +0200

    First Js Examples

---
+++ JS/Example1/Example1.js
@@ -0,0 +1,6 @@
+var radius = +prompt("Read circle: ")
+const PI = 3.14;
+var perimeter = 2 * PI * radius;
+var area = perimeter * (radius ** 2);
+console.log("Perimeter: " + perimeter + " with area: " + area);
+
